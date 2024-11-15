53639459c5036ac91fa07d2bbdabf70f73845526
neketta300 <neketta300@gmail.com>
2024-11-15 20:28:52 +0300

    added log reg func

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,10 @@
 import 'package:buble_tea_app/components/mu_buttons.dart';
 import 'package:buble_tea_app/components/my_textfields.dart';
+import 'package:buble_tea_app/ffi/native_check_logpas.dart';
 import 'package:buble_tea_app/pages/home_page.dart';
+import 'package:buble_tea_app/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:ffi/ffi.dart';
 
 class LoginPage extends StatefulWidget {
   final void Function()? onTap;
@@ -15,15 +18,40 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-
+  String? erorrText;
   // login
   void login() async {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const HomePage(),
-      ),
-    );
+    final login = emailController.text.toNativeUtf8();
+    final password = passwordController.text.toNativeUtf8();
+    final resultOfCppFunc = checkCredentials(login, password);
+    final _authService = AuthService();
+
+    if (resultOfCppFunc == 1) {
+      print('open app like admin');
+      erorrText = null;
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    } else {
+      erorrText = 'Не верный логин или пароль';
+      print('show error');
+    }
+    if (erorrText != null) {
+      try {
+        await _authService.signInWithEmailPassword(
+            emailController.text, passwordController.text);
+      } catch (e) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(e.toString()),
+          ),
+        );
+      }
+    }
   }
 
   @override
@@ -31,87 +59,89 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(175, 165, 153, 1),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // logo
-            Image.asset(
-              "assets/images/chokolate_bubble_tea.png",
-              height: 300,
-            ),
-
-            const SizedBox(
-              height: 70,
-            ),
-
-            // msg, app slogan
-            const Text(
-              "Boba is life",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // logo
+              Image.asset(
+                "assets/images/chokolate_bubble_tea.png",
+                height: 300,
               ),
-            ),
 
-            const SizedBox(
-              height: 25,
-            ),
+              const SizedBox(
+                height: 70,
+              ),
 
-            // email textfield
-            MyLogRegTextfield(
-              hintText: "Email",
-              obscureText: false,
-              controller: emailController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            // password textfield
-            MyLogRegTextfield(
-              hintText: "Password",
-              obscureText: true,
-              controller: passwordController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            // sign in buutton
-            MyPrimaryButton(
-              text: "Sign in",
-              onTap: login,
-            ),
-
-            const SizedBox(
-              height: 10,
-            ),
-            // not a member? reg now
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Not a member?",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 219, 212, 207),
-                  ),
+              // msg, app slogan
+              const Text(
+                "Boba is life",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.white,
                 ),
-                const SizedBox(
-                  width: 4,
-                ),
-                GestureDetector(
-                  onTap: widget.onTap,
-                  child: const Text(
-                    "Register now",
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // email textfield
+              MyLogRegTextfield(
+                hintText: "Email",
+                obscureText: false,
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // password textfield
+              MyLogRegTextfield(
+                hintText: "Password",
+                obscureText: true,
+                controller: passwordController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // sign in buutton
+              MyPrimaryButton(
+                text: "Sign in",
+                onTap: login,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+              // not a member? reg now
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Not a member?",
                     style: TextStyle(
                       color: Color.fromARGB(255, 219, 212, 207),
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  GestureDetector(
+                    onTap: widget.onTap,
+                    child: const Text(
+                      "Register now",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 219, 212, 207),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:buble_tea_app/components/mu_buttons.dart';
 import 'package:buble_tea_app/components/my_textfields.dart';
+import 'package:buble_tea_app/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -18,105 +19,130 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController confirmPasswordController =
       TextEditingController();
 
-  void register() async {}
+  void register() async {
+    final _authService = AuthService();
+
+    if (passwordController.text == confirmPasswordController.text) {
+      try {
+        await _authService.signUpWithEmailPassword(
+            emailController.text, passwordController.text);
+      } catch (e) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(e.toString()),
+          ),
+        );
+      }
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(
+          title: Text("Password dosn't match"),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(175, 165, 153, 1),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // logo
-            Icon(
-              Icons.lock_open_rounded,
-              size: 100,
-              color: Colors.grey[300],
-            ),
-
-            const SizedBox(
-              height: 25,
-            ),
-
-            // msg, app slogan
-            const Text(
-              "Let's create account for you",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // logo
+              Icon(
+                Icons.lock_open_rounded,
+                size: 100,
+                color: Colors.grey[300],
               ),
-            ),
 
-            const SizedBox(
-              height: 25,
-            ),
+              const SizedBox(
+                height: 25,
+              ),
 
-            // email textfield
-            MyLogRegTextfield(
-              hintText: "Email",
-              obscureText: false,
-              controller: emailController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            // password textfield
-            MyLogRegTextfield(
-              hintText: "Password",
-              obscureText: true,
-              controller: passwordController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            // confirm password textfield
-            MyLogRegTextfield(
-              hintText: "Confirm passwod",
-              obscureText: true,
-              controller: confirmPasswordController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            // sign in buutton
-            MyPrimaryButton(
-              text: "Sign up",
-              onTap: () => register(),
-            ),
-
-            const SizedBox(
-              height: 10,
-            ),
-
-            // already have an account? sign in
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Already have an account?",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 219, 212, 207),
-                  ),
+              // msg, app slogan
+              const Text(
+                "Let's create account for you",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                const SizedBox(
-                  width: 4,
-                ),
-                GestureDetector(
-                  onTap: widget.onTap,
-                  child: const Text(
-                    "Login now",
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // email textfield
+              MyLogRegTextfield(
+                hintText: "Email",
+                obscureText: false,
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // password textfield
+              MyLogRegTextfield(
+                hintText: "Password",
+                obscureText: true,
+                controller: passwordController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // confirm password textfield
+              MyLogRegTextfield(
+                hintText: "Confirm passwod",
+                obscureText: true,
+                controller: confirmPasswordController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // sign in buutton
+              MyPrimaryButton(
+                text: "Sign up",
+                onTap: () => register(),
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              // already have an account? sign in
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an account?",
                     style: TextStyle(
                       color: Color.fromARGB(255, 219, 212, 207),
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  GestureDetector(
+                    onTap: widget.onTap,
+                    child: const Text(
+                      "Login now",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 219, 212, 207),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
